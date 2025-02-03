75bcc34da02e2dafe8a8f49ea9d7b72a5034ae8e
CresthaSabin <emo.love8565@gmail.com>
2025-02-03 21:15:05 +0545

    new changes

--- Locator/Locator.py
+++ Locator/Locator.py
@@ -1,8 +1,15 @@
+class Locate:
+    """Stores all locators used in the framework."""
 
+    # Login Page Locators
+    nav_signin_xpath = '//*[@id="header"]/div[2]/div/div/nav/div[1]/a'
+    email_field_id = "email"
+    password_field_id = "passwd"
+    login_btn_xpath = '//*[@id="SubmitLogin"]/span'
 
-class Locate :
-    url = "https://gb.bishalkarki.com/index.php"
-    signin_xpath='//*[@id="header"]/div[2]/div/div/nav/div[1]/a'
-    username_id="email"
-    pswd_id="passwd"
-    signin_btn_xpath='//*[@id="SubmitLogin"]/span'
+    # Home Page
+    homepage_logo_xpath = '//*[@id="header_logo"]/a/img'
+
+    # Add to Cart Locators
+    product1_xpath = '//*[@id="blocknewproducts"]/li[1]/div/div[1]/div/a[1]/img'
+    addtocart_xpath = '//*[@id="blocknewproducts"]/li[1]/div/div[2]/div[2]/a[1]/span'
--- Pages/Login.py
+++ Pages/Login.py
@@ -5,32 +5,34 @@ from Locator.Locator import Locate
 from selenium.webdriver.common.by import By
 
 
-class Login:
+class Signin:
     def __init__(self,driver):
          self.driver = driver
          self.lc = Locate()
 
 
 
-    def SigninPage(self,username,password):
+    def SigninPage(self):
         driver = self.driver
         lc = self.lc
 
         #Click Sign in
-        sign_in = driver.find_element(By.XPATH,lc.signin_xpath)
+        sign_in = driver.find_element(By.XPATH,lc.nav_signin_xpath)
         sign_in.click()
 
+    def credential(self, username, password):
         #Enter Username
-        driver.implicitly_wait(10)
-        user_name = driver.find_element(By.ID,lc.username_id)
+        self.driver.implicitly_wait(10)
+        user_name = self.driver.find_element(By.ID,self.lc.email_field_id)
         user_name.send_keys(username)
 
         #Enter password
-        pwd_word = driver.find_element(By.ID,lc.pswd_id)
+        pwd_word = self.driver.find_element(By.ID,self.lc.password_field_id)
         pwd_word.send_keys(password)
 
+    def login(self):
         #Click sign in button
-        signin_btn = driver.find_element(By.XPATH,lc.signin_btn_xpath)
+        signin_btn = self.driver.find_element(By.XPATH,self.lc.login_btn_xpath)
         signin_btn.click()
         time.sleep(10)
 
