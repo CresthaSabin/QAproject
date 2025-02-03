f6f33e5c8027440104e970b3ed01b1bf38c1ad9d
CresthaSabin <emo.love8565@gmail.com>
2025-02-03 11:33:45 +0545

    new changes

--- Pages/Login.py
+++ Pages/Login.py
@@ -11,18 +11,28 @@ class Login:
          self.lc = Locate()
 
 
+
     def SigninPage(self,username,password):
         driver = self.driver
         lc = self.lc
+
+        #Click Sign in
         sign_in = driver.find_element(By.XPATH,lc.signin_xpath)
         sign_in.click()
+
+        #Enter Username
         driver.implicitly_wait(10)
         user_name = driver.find_element(By.ID,lc.username_id)
         user_name.send_keys(username)
+
+        #Enter password
         pwd_word = driver.find_element(By.ID,lc.pswd_id)
         pwd_word.send_keys(password)
+
+        #Click sign in button
         signin_btn = driver.find_element(By.XPATH,lc.signin_btn_xpath)
         signin_btn.click()
         time.sleep(10)
+
 
 
